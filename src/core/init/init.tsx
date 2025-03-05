a5d8dedd7bcaf9d5dcb8387ff4de2f4463d8ec9c
nik00did <02.ni.03.did.00@gmail.com>
2025-03-05 22:16:03 +0100

    PT-1 set up base project

---
+++ src/core/init/init.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "../../App/App";
+
+const init = (): void => {
+  const rootElement = document.getElementById("root");
+
+  if (!rootElement) {
+    return;
+  }
+
+  const root = createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
+
+export default init;
